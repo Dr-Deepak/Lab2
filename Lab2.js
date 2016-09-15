bb1ded5cba52cc960b596a0a675291ea903da9a9
Dr-Deepak <deepak.sharmacpa@gmail.com>
2016-09-15 14:55:02 -0400

    Updated version of code

--- Lab2.js
+++ Lab2.js
@@ -11,3 +11,32 @@ console.log("paper");
 else if(computerChoice > 0.67 && computerChoice < 1){
 console.log("scissors");
 }
+var compare = function(choice1, choice2){
+  if (choice1 ===choice2){
+        return "The result is a tie!";
+    }
+    else if (choice1 === "rock"){
+        if(choice2 === "scissors"){
+            return "rock wins";
+        }
+        else {
+            return "paper wins";
+        }
+    }else if (choice1 === "scissors"){
+        if(choice2 === "rock"){
+            return "rock wins";
+        }
+        else {
+            return "scissors wins";
+        }
+      }
+    else {
+      if(choice2 === "rock"){
+          return "paper wins";
+      }
+      else {
+          return "scissors wins";
+      }
+
+    }
+};
